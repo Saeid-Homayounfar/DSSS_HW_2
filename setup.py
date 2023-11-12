f4a1ef4c6b7e596742fa73fac007ba1492e9a32b
Saeid-Homayounfar <150022592+Saeid-Homayounfar@users.noreply.github.com>
2023-11-12 21:31:56 +0330

    Setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='Math_Quiz_For_DSSS_HW2',
-    version='1.2.3',
-    author='Saeid Homayounfar',
+    version='1.0.0',
+    author='Saeid_Homayounfar',
     packages=find_packages(),
-    install_requires=[
+    install_requires=['python'
     ],
 )
